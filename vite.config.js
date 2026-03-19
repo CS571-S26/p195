099bfad365258fc4231e0c6f217f833e0a652314
Timothy Chen <thchen3@wisc.edu>
2026-03-18 20:55:50 -0500

    changed base to /p195/, performed run build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/p195/',
   plugins: [react()],
   build: {
     outDir: "docs"
