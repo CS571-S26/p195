02a24e4f1ac306a1ce3df4119ae54b181b5767b5
Timothy Chen <thchen3@wisc.edu>
2026-03-18 20:52:45 -0500

    changed base to ./

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/p195/',
+  base: './',
   plugins: [react()],
   build: {
     outDir: "docs"
